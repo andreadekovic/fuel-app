fdce1b5df12ac9037d84a6ecc61b3debf4076b9c
Andrea Dekovic <andreadekovic@Andreas-MacBook-Pro.local>
2026-04-15 17:05:44 -0600

    Fix Solana build config

--- src/src/solana.js
+++ src/src/solana.js
@@ -1,15 +1,8 @@
-import { Connection, Keypair, clusterApiUrl } from "@solana/web3.js";
-
-const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-
 export async function createProjectWallet() {
+  const { Keypair } = await import("@solana/web3.js");
   const keypair = Keypair.generate();
   return {
     address: keypair.publicKey.toString(),
     privateKey: Array.from(keypair.secretKey),
   };
-}
-
-export function getConnection() {
-  return connection;
 }
