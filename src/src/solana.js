2a0fe5b11424f73b2aed7243455e1274f91009ef
Andrea Dekovic <andreadekovic@Andreas-MacBook-Pro.local>
2026-04-16 11:48:46 -0600

    Remove solana dependency, use mock wallet

--- src/src/solana.js
+++ src/src/solana.js
@@ -1,8 +1,8 @@
 export async function createProjectWallet() {
-  const { Keypair } = await import("@solana/web3.js");
-  const keypair = Keypair.generate();
+  const chars = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghjkmnpqrstuvwxyz123456789";
+  const random = (len) => Array.from({length: len}, () => chars[Math.floor(Math.random() * chars.length)]).join("");
   return {
-    address: keypair.publicKey.toString(),
-    privateKey: Array.from(keypair.secretKey),
+    address: random(44),
+    privateKey: Array.from({length: 64}, () => Math.floor(Math.random() * 256)),
   };
 }
